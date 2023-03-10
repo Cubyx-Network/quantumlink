aa6eb77d91031f6d9f778669991841f1651590fe
Sachsenspielt <sachsenspielt3@gmail.com>
2023-03-10 18:58:14 +0100

    feat: new list output -> old was limited to 2.000 chars

--- src/discordbot/commands/listLinks.ts
+++ src/discordbot/commands/listLinks.ts
@@ -1,11 +1,11 @@
 import {
   CommandInteraction,
+  EmbedBuilder,
   PermissionsBitField,
   SlashCommandBuilder,
 } from "discord.js";
 import ISlashCommand from "../ISlashCommand";
 import { getLinks } from "../../admin";
-import { table } from "table";
 
 export default {
   data: new SlashCommandBuilder()
@@ -14,13 +14,56 @@ export default {
     .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
   async execute(interaction: CommandInteraction) {
     const links = await getLinks();
-    const tableInput = [
-      ["ID", "URL", "Beschreibung"],
-      ...links.map((link) => {
-        return [link.id, link.url, link.description || ""];
-      }),
-    ];
+    // const tableInput = [
+    //   ["ID", "URL", "Beschreibung"],
+    //   ...links.map((link) => {
+    //     return [link.id, link.url, link.description || ""];
+    //   }),
+    // ];
 
-    await interaction.reply(`\`\`\`${table(tableInput)}\`\`\``);
+    const embeds = [];
+    const fields = [];
+
+    for (const link of links) {
+      if (fields.length % 3 === 0) {
+        fields.push({
+          name: "\u200b",
+          value: "\u200b",
+          inline: true,
+        });
+      }
+
+      fields.push({
+        name: link.id,
+        value: `[Link](${link.url})`,
+        inline: true,
+      });
+    }
+
+    const fieldChunks = chunkArray(fields, 24);
+
+    for (const fieldChunk of fieldChunks) {
+      const embed = new EmbedBuilder()
+        .setTitle("Linkübersicht")
+        .setDescription("Übersicht über alle QuantumLinks.")
+        .setColor(0xffffff)
+        .setFooter({
+          text: "------------------------------------------------------------------------------------------------",
+        })
+        .setFields(fieldChunk);
+      embeds.push(embed);
+    }
+
+    await interaction.reply({ embeds });
   },
 } as ISlashCommand;
+
+function chunkArray<T>(myArray: T[], chunk_size: number): T[][] {
+  let results = [];
+
+  while (myArray.length) {
+    results.push(myArray.splice(0, chunk_size));
+  }
+
+  return results;
+}
